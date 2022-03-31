9dbdbc011a9403fc73b5781f427d14f67c711632
abdes <abdes@users.noreply.github.com>
2022-03-31 09:01:39 +0000

    deploy: 47c9a0b0853a77befd5e82782ff2f4e162fce443

--- asap_contract_master/html/_static/documentation_options.js
+++ asap_contract_master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1 (8bf0bf53dfef)',
+    VERSION: '1.0.1 (47c9a0b0853a)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -8,5 +8,7 @@ var DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
