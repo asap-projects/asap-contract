404eecc2260057b4ebcadbe4bb84cda9c9aa1119
abdes <abdes@users.noreply.github.com>
2023-07-14 20:07:21 +0000

    deploy: 47c6d9fcd99990ffe7d9de060c7b96e668cd8a2a

--- asap_contract_master/html/_static/documentation_options.js
+++ asap_contract_master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.2 (14fc7a4825ab)',
+    VERSION: '1.0.2 (47c6d9fcd999)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
